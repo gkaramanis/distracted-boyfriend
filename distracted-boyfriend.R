08192f100558982c8cff5505a4f4e2ae156f1df8
Georgios <georgios@Georgios-iMac.local>
2020-04-24 18:17:37 +0200

    Final distraction

--- distracted-boyfriend.R
+++ distracted-boyfriend.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(ggpattern)
 
 clothes_1 <- tibble(
   x = c(6:18),
@@ -22,9 +23,9 @@ hair_1 <- tibble(
 )
 
 clothes_2 <-tibble(
-  x = c(22:34),
-  y = 0,
-  h = c(1, 4, 14, 18, 18, 19, 20, 20, 18, 17, 16, 16, 11),
+  x = c(22:34, 30, 31),
+  y = c(rep(0, 6), 5, 10, 10, 10, 0, 0, 0, 0, 0),
+  h = c(1, 4, 14, 18, 18, 19, 15, 10, 8, 7, 16, 16, 11, 4, 6),
   colour = "#4771B7"
 )
 
@@ -64,24 +65,35 @@ skin_3 <- tibble(
 )
 
 person_1 <- rbind(clothes_1, skin_1, hair_1)
-person_2 <- rbind(clothes_2, hair_2, skin_2)
+person_2 <- rbind(hair_2, clothes_2, skin_2)
 person_3 <- rbind(hair_3, clothes_3, skin_3)
 
 people <- do.call("rbind", list(person_1, person_2, person_3))
 
 ggplot(people) +
-  geom_segment(aes(x = x, y = y, xend = x, yend = y + h, colour = colour), size = 8.3) +
-  scale_color_identity(breaks = c("#A8B7C2", "#4771B7", "#E93223"),
+  # everything 
+  geom_tile(aes(x = x, y = y + h/2, height = h, fill = colour), size = 8.3, show.legend = FALSE) +
+  # just shirt_2
+  geom_tile_pattern(data = clothes_2,
+                    aes(x = x, y = y + h/2, height = h, fill = colour), size = 8.3,
+                    show.legend = FALSE,
+                    pattern = "crosshatch",
+                    pattern_colour  = NA, pattern_fill = "black",
+                    pattern_angle = 0,
+                    pattern_density = 0.1, pattern_spacing = 0.014
+  ) +
+  geom_segment(aes(x = 0, y = 0, xend = 0, yend = 0, colour = colour), size = 0) + # dummy for the legend
+  scale_colour_identity(breaks = c("#A8B7C2", "#4771B7", "#E93223"),
                        labels = c("Doing\nuseful data\nanalysis", "Me\n\n", "Making\nstupid ggplot\nbar charts"),
-                       guide = guide_legend(title = "",  keyheight = 7,
-                                            override.aes = list(size = 6))) +
+                       guide = guide_legend(title = "", keyheight = 6, override.aes = list(size = 6, pattern_fill = NA))) +
+  scale_fill_identity() +
   scale_x_continuous(breaks = c(0:42), expand = c(0, 0)) +
   scale_y_continuous(breaks = c(seq(0, 30, 5)), minor_breaks = c(0:30), expand = c(0, 0)) +
   labs(title = "Chart Title") +
   coord_fixed(xlim = c(0.5, 42.5), ylim = c(0, 30)) +
   theme_linedraw(base_family = "Calibri") +
   theme(
-    legend.text = element_text(size = 18, vjust = -0.3, lineheight = 1.3),
+    legend.text = element_text(size = 18, vjust = -0.6, lineheight = 1.2),
     legend.margin = margin(0, 0, 80, 0),
     plot.background = element_rect(fill = "white", colour = NA),
     panel.border = element_rect(colour = "grey70"),
